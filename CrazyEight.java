48138bb01644c9d2446c18e8fe36514cd50ac39d
cfoo20 <cfoo20@punahou.edu>
2020-02-21 18:16:18 +0000

    first commit

---
+++ CrazyEight.java
@@ -0,0 +1,25 @@
+// Crazy Eight by Cheyenne Foo
+public class CrazyEight
+{
+    // Instance variable
+    private String suit;
+    private String number;
+    private int hand;
+
+    // Constructor
+    public card (String suit, String number)
+    {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    // Methods
+    public shuffle ()
+    {
+        
+    }
+    public getCard ()
+    {
+
+    }
+}
